d027e1513b4662c1f75bff4e8fdce5614f9599bc
“Spencer <spencer.richards@mx.com>
2023-04-03 16:55:08 -0600

    sets up revocation strategy with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  # Strategy that compares both `jti` values
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
